677c58f7c1986a1f62a363c64560cf49bfbde1d4
cc <2627865838@qq.com>
2023-11-02 15:57:02 +0800

    novel1.1

--- src/main/java/com/cc/controller/ChapterController.java
+++ src/main/java/com/cc/controller/ChapterController.java
@@ -30,6 +30,7 @@ public class ChapterController {
     @GetMapping("/getChapter/{novelId}/{chapterNum}")
     @ResponseBody
     public Chapter getChapter(@PathVariable("novelId") int novelId, @PathVariable("chapterNum") int chapterNum) throws JsonProcessingException {
+        System.out.println("test");
         try {
             return chapterService.getChapterById(novelId, chapterNum);
         } catch (Exception e) {
